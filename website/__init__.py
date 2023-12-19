652168574fe08a0d9b382b09240607dc1b238428
kkosek-dev <kkosek@alum.utk.edu>
2023-12-19 10:16:35 -0500

    New starter code

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,8 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secretKeytemplate'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -20,9 +20,9 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-
-    #with app.app_context():
-    #    db.create_all()
+    
+    with app.app_context():
+        db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -34,8 +34,8 @@ def create_app():
 
     return app
 
+
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-    
